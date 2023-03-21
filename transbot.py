e1d34be4132b33b15b8e76763f478c257613460a
slkom2 <slkom2@gmail.com>
2023-03-21 11:51:48 +0300

    beta1

--- transbot.py
+++ transbot.py
@@ -5,11 +5,13 @@ bot = telebot.TeleBot('6263282365:AAHyuz23Pmnt1cCzyXYoDnvugp_2d41tczI')
 # Функция, обрабатывающая команду /start
 @bot.message_handler(commands=["start"])
 def start(m, res=False):
-    bot.send_message(m.chat.id, 'Русско-черкесский переводчик.\nНаберите слово на русском языке...')
+    bot.send_message(m.chat.id, 'Русско-черкесский бот-переводчик.\n'
+                                'ver 0.1(beta). Источник словарной базы: academic.ru\n\n'
+                                'Наберите слово на русском языке...')
 # Получение сообщений от юзера
 @bot.message_handler(content_types=["text"])
 def handle_text(message):
-    bot.send_message(message.chat.id, (scrap.translate(message.text)).capitalize())
+    bot.send_message(message.chat.id, (scrap.translate(message.text)).isupper())
     # bot.send_message(message.chat.id, 'Перевод: ' + (scrap.translate(message.text)).title())
     # bot.send_message(message.chat.id, f'Длина вашего сообщения: {len(message.text)}')
 # Запускаем бота
