f282b14c50ed1f6479070d732f07f52207073ab3
TarumK <76095818+TarumK@users.noreply.github.com>
2023-03-21 12:05:26 +0300

    Update transbot.py

--- transbot.py
+++ transbot.py
@@ -5,13 +5,16 @@ bot = telebot.TeleBot('6263282365:AAHyuz23Pmnt1cCzyXYoDnvugp_2d41tczI')
 # Функция, обрабатывающая команду /start
 @bot.message_handler(commands=["start"])
 def start(m, res=False):
-    bot.send_message(m.chat.id, 'Русско-черкесский бот-переводчик.\n'
-                                'ver 0.1(beta). Источник словарной базы: academic.ru\n\n'
+    bot.send_message(m.chat.id, 'Русско-черкесский бот-переводчик. ver 0.1(beta).\n'
+                                'Источник словарной базы: academic.ru\n\n'
+                                'Здравствуйте! Я всего лишь языковая модель Жамбот'
+                                'и могу допускать ошибки в переводах, но я быстро учусь.\n'
+                                
                                 'Наберите слово на русском языке...')
 # Получение сообщений от юзера
 @bot.message_handler(content_types=["text"])
 def handle_text(message):
-    bot.send_message(message.chat.id, (scrap.translate(message.text)).isupper())
+    bot.send_message(message.chat.id, (scrap.translate(message.text)).upper())
     # bot.send_message(message.chat.id, 'Перевод: ' + (scrap.translate(message.text)).title())
     # bot.send_message(message.chat.id, f'Длина вашего сообщения: {len(message.text)}')
 # Запускаем бота
